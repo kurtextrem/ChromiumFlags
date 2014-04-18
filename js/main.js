7bc7a96dbfcb29f4282ad748c42b1b24d75696e1
Jacob Groß (kurtextrem) <kurtextrem@gmail.com>
2014-04-18 10:23:15 +0200

    Added favicon; made switches focusable

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@
 						add = ' <span class="label label-primary">new</span>'
 					if (v.deleted)
 						add = '<span class="label label-danger">deleted</span>'
-					html += '<td class="switch" id="' + encodeURIComponent(i) + '">' + i + add + ' <a href="#' + encodeURIComponent(i) + '" class="anchor">#</a></td>'
+					html += '<td class="switch" id="' + encodeURIComponent(i) + '" tabindex="0">' + i + add + ' <a  class="anchor" href="#' + encodeURIComponent(i) + '">#</a></td>'
 					html += '<td>' + v.comment.replace(/[^:](\/\/.+)/g, '<br><span class="text-muted">$1</span>').replace(/(\. |:(?!\/| |[A-Z0-9:]))/g, '$1<br>').replace(/  /g, '<br>&nbsp;&nbsp;').replace(/(<br>&nbsp;&nbsp;){3}/g, '').replace(/--((\w|-)+)/g, '<a href="#$1">--$1</a>') + '</td>' // only break after ":char", as ": " probably tells there is something in the same line after it
 					html += '</tr>'
 				}.bind(this))
@@ -83,14 +83,16 @@
 
 		registerEvents: function() {
 			window.setTimeout(function() {
-				$('[data-toggle="tooltip"]').tooltip()
-				$('[data-show="true"]').tooltip('show')
+				//$('[data-toggle="tooltip"]').tooltip()
+				//$('[data-show="true"]').tooltip('show')
 			}, 2000) // hurts performance so much
 			if (location.hash !== '') {
 				var elem = $(location.hash)
 				$('html').animate({
 				        scrollTop: elem.offset().top
-				}, 0) // workaround for hash not working
+				}, 0, function() {
+					elem.focus()
+				}) // workaround for hash not working
 				//elem.parent().addClass('target')
 			}
 			// http://www.jitbit.com/alexblog/230-javascript-injecting-extra-info-to-copypasted-text/
